a4ffededffa60d2931e8a118d02101f55aa981aa
Jonathan <lalilaloe@hotmail.com>
2021-03-08 16:34:41 +0100

    Fix launching cmd or powershell from wsl path & added before/after clear

--- index.js
+++ index.js
@@ -39,40 +39,78 @@ function wtArguments(options) {
   if (options.title) wtArgs.push(`--title "${options.title}"`);
   if (options.profile)
     wtArgs.push(`-p "${getProfileByOption(options.profile).name}"`);
-  if (options.dir) wtArgs.push(`-d ${options.dir}`);
   if (options.color) wtArgs.push(`--tabColor "${options.color}"`);
   if (options.d) wtArgs.push(`-d "${options.d}"`);
   return wtArgs.join(" ");
 }
 
+function clearAfter(options) {
+  return options.q && options.q !== "before";
+}
+
 function execute(command, options) {
   let fullCommand;
+  if (options.q === "before") options.clear = "clear";
   if (options.profile === "cmd") {
-    // Trick to continue using the terminal in Command Prompt
+    // Trick to continue using the terminal in Command Prompt and keep command visible
     // 'cmd /k' - prevents autoclosing after running command
     // 'ECHO=commmand|cmd' - echoes the command and executes it in the terminal,
     //    right after executing/echoeing it runs cmd
     // '&& cmd>nul' prevents output of last executed cmd command
     //    on windows an empty line is supressed and on unix the default cmd greeting
-    command = `cmd /k "ECHO=${command}|cmd  && ${
-      options.q ? "cls" : "cmd>nul"
-    }"`;
+    if (options.q === "before") {
+      // Do not use this trick on user request
+      command = `cmd /k "${command}"`;
+    } else {
+      command = `cmd /k "ECHO=${command}|cmd && ${
+        clearAfter(options) ? "cls" : "cmd>nul"
+      };"`;
+    }
   }
   if (options.profile === "powershell") {
     // The same trick is used for Powershell
     // Where Write-Output === echo, -noexit === cmd /k, | out-null === >nul
-    command = `powershell -noexit "Write-Output ${command}|powershell -noexit\\;${
-      options.q ? "clear" : "powershell | out-null"
-    } "`;
+    if (options.q === "before") {
+      // Do not use this trick on user request
+      command = `powershell -noexit "${command}"`;
+    } else {
+      command = `powershell -noexit "Write-Output ${command}|powershell -noexit\\;${
+        clearAfter(options) ? "clear" : "powershell | out-null"
+      } "`;
+    }
   }
   if (options.profile === "wsl") {
-    command = `bash -c "echo ${command} && ${command}\\;${
-      options.q ? "clear\\;" : ""
-    } exec bash 2>&1"`;
+    // Use an equevalant trick for wsl bash to keep command visible
+    if (options.q === "before") {
+      command = `bash -c "cd ${options.d} && ${command} && exec bash 2>&1"`;
+    } else {
+      command = `bash -c "cd ${options.d} && echo ${command} && ${command}\\;${
+        clearAfter(options) ? "clear\\;" : ""
+      } exec bash 2>&1"`;
+    }
+
+    options.d = undefined; // Unset directory because we use cd <dir...>
   }
   if (isWin) {
     fullCommand = `${wt(options)} ${wtArguments(options)} ${command}`;
   } else {
+    if (options.profile !== "wsl" && options.d.match(/^\/mnt\//)) {
+      // Redirect wsl disk mount it's Windows path
+      options.d = options.d.replace(/^\/mnt\//, "");
+      const diskLetter = options.d
+        .match(/[^\/]*/)
+        .shift()
+        .toUpperCase();
+      options.d = options.d.replace(/[^\/]*/, `${diskLetter}:`);
+    } else if (options.profile !== "wsl" && options.d.match(/^\//)) {
+      // Redirect to wsl path in windows
+      const currentWslDistroName = execSync(`wsl.exe -l -q --running`)
+        .toString()
+        .replace(/\r?\n|\r/g, "");
+      const wslPathPrefix = `\\\\wsl$\\${currentWslDistroName}`;
+      options.d = wslPathPrefix.trim() + options.d.replace(/\//g, "\\");
+    }
+
     fullCommand = `cmd.exe /c ${wt(options)} ${wtArguments(
       options
     )} ${command}`; // Launch from unix
@@ -143,7 +181,7 @@ program
   )
   .option("-s, --settings <settings>", "Assign a settings set (profile).")
   .option("-t, --title <title>", "Specify title for new tab.")
-  .option("-q", "Clear the new tab's screen.")
+  .option("-q [before]", "Clear the new tab's screen. default (after)")
   .option(
     "-d <dir>",
     "Specify working directory; -d '' disables inheriting the current dir.",
@@ -166,7 +204,6 @@ program
   // First versions specific
   .option("--debug", "Enable debugging, outputs the command executed")
   .action((cmd, options, command) => {
-    console.log(cmd);
     if (options.profile && typeof options.profile !== "string") {
       console.log(`Available profiles:
       - ${availableProfiles().join("\n      - ")}`);
