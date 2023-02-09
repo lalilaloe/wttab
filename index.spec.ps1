e72c650c75920944e7a61ad8bf40e65f2ab7bb58
lalilaloe <lalilaloe@hotmail.com>
2023-02-09 12:41:24 +0100

    feat(test): Add simple test cases, powershell initial

---
+++ index.spec.ps1
@@ -0,0 +1,26 @@
+# make sure wttab is installed on windows: npm i -g .
+# for testing docs see: https://powershellmagazine.com/2014/03/27/testing-your-powershell-scripts-with-pester-assertions-and-more/
+Describe 'Run command' {
+     Context 'Runs help command'    {
+        $output = & wttab 2>&1
+        $stderr = $output | ?{ $_ -is [System.Management.Automation.ErrorRecord] }
+        $stdout = $output | ?{ $_ -isnot [System.Management.Automation.ErrorRecord] }
+        
+        It 'Shows wttab Help'  {
+            $stderr | Should BeNullOrEmpty
+            $stdout -eq "Usage: index [options] [cmd...]" | Should Be $true
+        }
+    }
+
+    Context 'Runs single command'    {
+        $output = & wttab -t 'test case' npm run script 2>&1
+        $stderr = $output | ?{ $_ -is [System.Management.Automation.ErrorRecord] }
+        $stdout = $output | ?{ $_ -isnot [System.Management.Automation.ErrorRecord] }
+        #test one
+        It 'Executes succesfully'  {
+            $stderr | Should BeNullOrEmpty
+            $stdout | Should Be "c"
+            (Get-Process).ProcessName | %{ if($_ -imatch 'WindowsTerminal') { $_ | Should Be $true } }
+        }
+    }
+}
